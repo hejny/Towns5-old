cb1eb720926199e9c9dfed77e17d8facc54878e1
Stefan Kecskes <mr.kecskes@gmail.com>
2015-10-22 17:29:52 +0100

    [Frontend][SK] progressing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,47 @@
+var gulp        = require('gulp');
+var minifyHTML  = require('gulp-minify-html');
+var uglify      = require('gulp-uglify');
+var minifyCss   = require('gulp-minify-css');
+var concat      = require('gulp-concat');
+var jsdoc       = require("gulp-jsdoc");
 
 
-var gulp = require('gulp');
-var minifyHTML = require('gulp-minify-html');
-var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
-
-var concat = require('gulp-concat');
-
-var jsdoc = require("gulp-jsdoc");
-
-gulp.task('build', function() {
-
-
-
-
-
-
-
+gulp.task('html', function () {
     gulp.src('app/*.html')
         .pipe(minifyHTML({
             conditionals: true,
             spare:true
         }))
         .pipe(gulp.dest('app-dist/'));
+});
 
-
+gulp.task('scripts', function () {
     gulp.src('app/*/*.js')
         .pipe(uglify())
         .pipe(concat('towns.js'))
         .pipe(gulp.dest('app-dist/'));
 
+    gulp.src("app/js/*.js")
+        .pipe(jsdoc('documentation/'));
 
+    /*var jsdoc = require("gulp-jsdoc");
+
+     gulp.src("./src/*.js")
+     .pipe(jsdoc('./documentation-output'))*/
+});
+
+gulp.task('css', function () {
     gulp.src('app/*/*.css')
         .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(concat('towns.css'))
         .pipe(gulp.dest('app-dist/'));
-
-
-
-
-    gulp.src("app/js/*.js")
-        .pipe(jsdoc('documentation/'));
-
-
-
-    return;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
 
+gulp.task('default', ['html', 'scripts', 'css'], function() {
 
+    console.log(' ¯\\_(ツ)_/¯ Hotovo, teraz uz len checkujem zmeny ');
+    gulp.watch('app/*.{htm,html}', ['html']);
+    gulp.watch('app/*/*.js', ['scripts']);
+    gulp.watch('app/*/*.css', ['css']);
 
-/*var jsdoc = require("gulp-jsdoc");
-
- gulp.src("./src/*.js")
- .pipe(jsdoc('./documentation-output'))*/
+});
