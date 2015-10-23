6be92011a9d0e4597c4e5c1728104d36db4d24cc
Stefan Kecskes <mr.kecskes@gmail.com>
2015-10-23 16:50:36 +0100

    [Frontend][SK] progressing gulpfile - rozdelenie buildov na dev a na produkcne. Pridany js syntax tester: gulp lint

--- app/js/events.js
+++ app/js/events.js
@@ -233,8 +233,8 @@ $(function() {
 
 
 
-        }
-        , 100
+        },
+        100
     );
 
 
@@ -244,7 +244,7 @@ $(function() {
 
         //e.preDefault();
 
-        if(terrainChanging!=false){
+        if(terrainChanging!==false){
 
             if(e.deltaY>0){
                 selecting_distance+=100;
@@ -255,7 +255,7 @@ $(function() {
 
 
         }else
-        if(building!=false){
+        if(building!==false){
 
             if(e.deltaY>0){
                 building.rot+=10;
@@ -393,7 +393,7 @@ $(function() {
             if (window_opened)return;
 
 
-            if(BorderMoveX!=0 || BorderMoveY!=0){
+            if(BorderMoveX!==0 || BorderMoveY!==0){
 
                 BorderMoveDelay_-=100;
 
@@ -412,11 +412,11 @@ $(function() {
 
     $(window).mouseleave(function() {// cursor has left the window
         BorderMoveX=0;BorderMoveY=0;
-    })
+    });
 
     $('body').mouseleave(function() {// cursor has left the IE window
         BorderMoveX=0;BorderMoveY=0;
-    })
+    });
 
 
     var mouseMove=function (e) {
@@ -711,7 +711,7 @@ $(function() {
 
 
                 if (l < 2 && ll<2) {
-                    var rot = tmp.rot;
+                    rot = tmp.rot;
 
                 } else {
 
