c688b2f1ea294f737403dc6e5741a238977ba814
Stefan Kecskes <mr.kecskes@gmail.com>
2015-11-02 16:04:39 +0000

    [FRONTEND][SK] autozarovnanie

--- app/index.php
+++ app/index.php
@@ -1,21 +1,20 @@
 <?php
 /**
-
-    ██╗███╗   ██╗██████╗ ███████╗██╗  ██╗
-    ██║████╗  ██║██╔══██╗██╔════╝╚██╗██╔╝
-    ██║██╔██╗ ██║██║  ██║█████╗   ╚███╔╝
-    ██║██║╚██╗██║██║  ██║██╔══╝   ██╔██╗
-    ██║██║ ╚████║██████╔╝███████╗██╔╝ ██╗
-    ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
-    © Towns.cz
-
+ *
+ * ██╗███╗   ██╗██████╗ ███████╗██╗  ██╗
+ * ██║████╗  ██║██╔══██╗██╔════╝╚██╗██╔╝
+ * ██║██╔██╗ ██║██║  ██║█████╗   ╚███╔╝
+ * ██║██║╚██╗██║██║  ██║██╔══╝   ██╔██╗
+ * ██║██║ ╚████║██████╔╝███████╗██╔╝ ██╗
+ * ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+ * © Towns.cz
  * @fileOverview This PHP file generates HTML skeleton for browser...
  */
 
 
 //======================================================================================================================
 
-    //todo zde by se mela analyzovat URI - poslat dotaz do towns API a pote naplnit informace nize podle toho.
+//todo zde by se mela analyzovat URI - poslat dotaz do towns API a pote naplnit informace nize podle toho.
 //todo zde by se mela analyzovat URI - poslat dotaz do towns API a pote naplnit informace nize podle toho.
 // TODO: Zgrupnut tieto premenne do jedneho pola $page a pouzivat ako $page['meta_og']['site_name'] alebo $page['title']. Takymto zgrupenim budeme vediet odkial tieto hodnoty su.
 $title = 'Towns';
@@ -47,47 +46,47 @@ http_response_code(200);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?=htmlspecialchars($title)?></title>
-    <meta name="description" content="<?=addslashes($description)?>">
+    <title><?= htmlspecialchars($title) ?></title>
+    <meta name="description" content="<?= addslashes($description) ?>">
     <link rel="icon" href="/favicon.ico">
 
     <?php
     //--------------------------------Dodatecne hlavicky
-    if(isset($config['facebook']['app_id'])) {
-        echo '<meta property="fb:app_id" content="'.$config['facebook']['app_id'].'" >';
+    if (isset($config['facebook']['app_id'])) {
+        echo '<meta property="fb:app_id" content="' . $config['facebook']['app_id'] . '" >';
     }
 
     //--------------------------------Open Graph informace
 
-    foreach($meta_og as $key=>$value){
-        echo('<meta property="fb:'.addslashes($key).'" content="'.addslashes($value).'" >');
+    foreach ($meta_og as $key => $value) {
+        echo('<meta property="fb:' . addslashes($key) . '" content="' . addslashes($value) . '" >');
 
     }
 
     //tady je podminka zda jse o testovaci verzi
-    if(isset($config['environment']) && $config['environment'] != "production") : ?>
+    if (isset($config['environment']) && $config['environment'] != "production") : ?>
 
     <link rel="stylesheet" type="text/css" href="/app/css/style.css"/>
 
     <link rel="stylesheet" type="text/css"
-          href="/app/css-lib/roboto-fontface.css?family=Roboto:400,700&subset=latin,latin-ext">
-    <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome-animation.css">
+          href="/app/css-lib/roboto-fontface.css?family=Roboto:400,700&subset=latin,latin-ext"/>
+    <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome.css"/>
+    <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome-animation.css"/>
 
 
         <script src="/app/js-lib/jquery.js"></script>
         <script src="/app/js-lib/jquery-ui.js"></script>
-        
+
         <script src="/app/js-lib/jquery.ui.touch-punch.js"></script>
         <script src="/app/js-lib/jquery.mousewheel.js"></script>
         <script src="/app/js-lib/hammer.js"></script>
         <script src="/app/js-lib/jquery.fullscreen.js"></script>
-        
-            <script src="/app/js/func.lib.js"></script>
-            <script src="/app/js/log.lib.js"></script>
-            <script src="/app/js/main.js"></script>
-            <script src="/app/js/lang.lib.js"></script>
-            <script src="/app/locale/cs.js"></script>
+
+        <script src="/app/js/func.lib.js"></script>
+        <script src="/app/js/log.lib.js"></script>
+        <script src="/app/js/main.js"></script>
+        <script src="/app/js/lang.lib.js"></script>
+        <script src="/app/locale/cs.js"></script>
 
 
         <script src="/app/js/townsapi.lib.js"></script>
@@ -108,10 +107,9 @@ http_response_code(200);
         <script src="/app/js/ui.js"></script>
 
 
-
-            <script src="/app/coffeehtml/debug.js"></script>
-            <!--<script src="app/coffeehtml/loginform.js"></script>-->
-            <script src="/app/coffeehtml/projects.js"></script>
+        <script src="/app/coffeehtml/debug.js"></script>
+        <!--<script src="app/coffeehtml/loginform.js"></script>-->
+        <script src="/app/coffeehtml/projects.js"></script>
 
 
         <script src="/app/js/functions/login.js"></script>
@@ -129,23 +127,27 @@ http_response_code(200);
 <body>
 
 
+<?php if (isset($config['google']['tracking_id'])) : ?>
+    <!-- Google Analytics -->
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
+        ga('create', '<?= $config['google']['tracking_id'] ?>', 'auto');
+        ga('send', 'pageview');
 
-        <?php if (isset($config['google']['tracking_id'])) : ?>
-            <!-- Google Analytics -->
-            <script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-                ga('create', '<?= $config['google']['tracking_id'] ?>', 'auto');
-                ga('send', 'pageview');
-
-            </script>
-            <!-- End Google Analytics -->
-        <?php endif; ?>
-
+    </script>
+    <!-- End Google Analytics -->
+<?php endif; ?>
 
 
 <div id="loading">
@@ -153,45 +155,37 @@ http_response_code(200);
 </div>
 
 
+<div id="map_drag"></div>
 
-        <div id="map_drag"></div>
-
-        <div id="loadbar_outer">
-            <div id="loadbar"></div>
-        </div>
-
+<div id="loadbar_outer">
+    <div id="loadbar"></div>
+</div>
 
 
-        <canvas id="map_buffer" width="100" height="100"></canvas>
-        <canvas id="map_bg" width="100" height="100"></canvas>
+<canvas id="map_buffer" width="100" height="100"></canvas>
+<canvas id="map_bg" width="100" height="100"></canvas>
 
 
-
-        <canvas id="selecting-distance" width="100" height="50"></canvas>
-
+<canvas id="selecting-distance" width="100" height="50"></canvas>
 
 
-        <div id="selecting-distance-ctl" style="display: none;">
-            <div id="selecting-distance-plus" class="mini-button"><i class="fa fa-plus"></i></div>
-            <div id="selecting-distance-minus" class="mini-button"><i class="fa fa-minus"></i></div>
-            <div id="selecting-distance-left" class="mini-button"><i class="fa fa-angle-double-left"></i></i></div>
-            <div id="selecting-distance-right" class="mini-button"><i class="fa fa-angle-double-right"></i></i></div>
-            <div id="selecting-distance-close" class="mini-button"><i class="fa fa-times"></i></div>
+<div id="selecting-distance-ctl" style="display: none;">
+    <div id="selecting-distance-plus" class="mini-button"><i class="fa fa-plus"></i></div>
+    <div id="selecting-distance-minus" class="mini-button"><i class="fa fa-minus"></i></div>
+    <div id="selecting-distance-left" class="mini-button"><i class="fa fa-angle-double-left"></i></i></div>
+    <div id="selecting-distance-right" class="mini-button"><i class="fa fa-angle-double-right"></i></i></div>
+    <div id="selecting-distance-close" class="mini-button"><i class="fa fa-times"></i></div>
 
 </div>
 
 
-
-
-
-        <nav class="menu">
+<nav class="menu">
 
     <!--todo [PH] vyřešit nějak lépe lokacizaci v aplikaci-->
     <div class="menu-logo">
         <img src="media/image/icon/logo1.png" alt="Towns.cz logo"/>
 
     </div>
-
 
 
     <ul class="menu-list menu-list-left">
@@ -238,7 +232,6 @@ http_response_code(200);
         </li>
 
 
-
     </ul>
 
     <ul class="menu-list menu-list-right">
@@ -249,10 +242,13 @@ http_response_code(200);
         </li>
 
 
-        <li class="menu-list-item menu-list-item-icon js-popup-notification-open faa-parent animated-hover"><i class="fa fa-flag fa-lg faa-shake"></i></li>
-        <li class="menu-list-item menu-list-item-icon faa-parent animated-hover"><i class="fa fa-wrench fa-lg faa-wrench"></i></li>
-        <li class="menu-list-item menu-list-item-icon faa-parent animated-hover" onclick="$(document).toggleFullScreen();"><i class="fa fa-arrows-alt fa-lg faa-pulse"></i></li><!--todo tohle neni ciste reseni js by mel byt mimo html dokumentu v tomhle pripade v ui.js-->
-
+        <li class="menu-list-item menu-list-item-icon js-popup-notification-open faa-parent animated-hover"><i
+                class="fa fa-flag fa-lg faa-shake"></i></li>
+        <li class="menu-list-item menu-list-item-icon faa-parent animated-hover"><i
+                class="fa fa-wrench fa-lg faa-wrench"></i></li>
+        <li class="menu-list-item menu-list-item-icon faa-parent animated-hover"
+            onclick="$(document).toggleFullScreen();"><i class="fa fa-arrows-alt fa-lg faa-pulse"></i></li>
+        <!--todo tohle neni ciste reseni js by mel byt mimo html dokumentu v tomhle pripade v ui.js-->
 
 
     </ul>
@@ -261,58 +257,41 @@ http_response_code(200);
 </nav>
 
 
-
-
-
-
-
 <aside id="objectmenu">
     <div id="objectmenu-inner">
         <div class="action-wrapper">
 
-                        <div class="action js-popup-action-open" template_params></div>
+            <div class="action js-popup-action-open" template_params></div>
 
-                    </div>
-                <div>
-            </aside>
-
-
+        </div>
+        <div>
+</aside>
 
 
+<div id="popup-action">
+    <div class="arrow"></div>
+    <div class="content"></div>
+    <div class="close js-popup-action-close"><i class="fa fa-times"></i></div>
+</div>
 
 
-            <div id="popup-action">
-                <div class="arrow"></div>
-                <div class="content"></div>
-                <div class="close js-popup-action-close"><i class="fa fa-times"></i></div>
-            </div>
+<div class="overlay" style="display: <?= addslashes($window_display) ?>;"></div>
 
-
-
-
-
-
-            <div class="overlay" style="display: <?=addslashes($window_display)?>;"></div>
-
-<div class="popup-window" style="display: <?=addslashes($window_display)?>;">
-    <div class="header"><?=addslashes($window_header)?></div>
-    <div class="content"><?=addslashes($window_content)?></div>
+<div class="popup-window" style="display: <?= addslashes($window_display) ?>;">
+    <div class="header"><?= addslashes($window_header) ?></div>
+    <div class="content"><?= addslashes($window_content) ?></div>
 
 
     <div class="close js-popup-window-close"><i class="fa fa-times"></i></div>
 </div>
 
 
+<div class="popup-notification">
+    <div class="arrow"></div>
+    <div class="header"></div>
+    <div class="content">
 
-
-
-
-            <div class="popup-notification">
-                <div class="arrow"></div>
-                <div class="header"></div>
-                <div class="content">
-
-        <?=htmlspecialchars($notifications_content)?>
+        <?= htmlspecialchars($notifications_content) ?>
 
     </div>
     <div class="footer">
@@ -321,9 +300,7 @@ http_response_code(200);
 </div>
 
 
-
-
-    </body>
+</body>
 </html>
 
 
