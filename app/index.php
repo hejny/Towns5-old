aa012d9c84a0ab0265b09af5a79bd1a80a419bbc
Stefan Kecskes <mr.kecskes@gmail.com>
2015-11-02 15:40:02 +0000

    [FRONTEND][SK] fixnuty fullscreen + dokonceny production build

--- app/index.php
+++ app/index.php
@@ -1,303 +1,303 @@
 <?php
 
 
-    //todo zde by se mela analyzovat URI - poslat dotaz do towns API a pote naplnit informace nize podle toho.
-    // TODO: Zgrupnut tieto premenne do jedneho pola $page a pouzivat ako $page['meta_og']['site_name'] alebo $page['title']. Takymto zgrupenim budeme vediet odkial tieto hodnoty su.
-    $title = 'Towns';
-    $description = '';
-    $meta_og = [
-        'site_name' => 'Towns',
-        'title' => $title,
-        'description' => $description,
-        'type' => 'game'
-        //'url' =>
-        //'image' =>
-    ];
+//todo zde by se mela analyzovat URI - poslat dotaz do towns API a pote naplnit informace nize podle toho.
+// TODO: Zgrupnut tieto premenne do jedneho pola $page a pouzivat ako $page['meta_og']['site_name'] alebo $page['title']. Takymto zgrupenim budeme vediet odkial tieto hodnoty su.
+$title = 'Towns';
+$description = '';
+$meta_og = [
+    'site_name' => 'Towns',
+    'title' => $title,
+    'description' => $description,
+    'type' => 'game'
+    //'url' =>
+    //'image' =>
+];
 
 
-    $window_display = 'none';
-    $window_header = '';
-    $window_content = '';
+$window_display = 'none';
+$window_header = '';
+$window_content = '';
 
-    $notifications_content = '';
+$notifications_content = '';
 
 
-    http_response_code(200);
+http_response_code(200);
 
 
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title><?=htmlspecialchars($title)?></title>
-        <meta name="description" content="<?=addslashes($description)?>">
-        <link rel="icon" href="/favicon.ico">
+    <title><?=htmlspecialchars($title)?></title>
+    <meta name="description" content="<?=addslashes($description)?>">
+    <link rel="icon" href="/favicon.ico">
 
-        <?php
-        //--------------------------------Dodatecne hlavicky
-        if(isset($config['facebook']['app_id'])) {
-            echo '<meta property="fb:app_id" content="'.$config['facebook']['app_id'].'" >';
-        }
+    <?php
+    //--------------------------------Dodatecne hlavicky
+    if(isset($config['facebook']['app_id'])) {
+        echo '<meta property="fb:app_id" content="'.$config['facebook']['app_id'].'" >';
+    }
 
-        //--------------------------------Open Graph informace
+    //--------------------------------Open Graph informace
 
-        foreach($meta_og as $key=>$value){
-            echo('<meta property="fb:'.addslashes($key).'" content="'.addslashes($value).'" >');
+    foreach($meta_og as $key=>$value){
+        echo('<meta property="fb:'.addslashes($key).'" content="'.addslashes($value).'" >');
 
-        }
+    }
 
-        //tady je podminka zda jse o testovaci verzi
-        if(isset($config['environment']) && $config['environment'] != "production") : ?>
+    //tady je podminka zda jse o testovaci verzi
+    if(isset($config['environment']) && $config['environment'] != "production") : ?>
 
-            <link rel="stylesheet" type="text/css" href="/app/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="/app/css/style.css"/>
 
-            <link rel="stylesheet" type="text/css"
-                  href="/app/css-lib/roboto-fontface.css?family=Roboto:400,700&subset=latin,latin-ext">
-            <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome.css">
-            <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome-animation.css">
-
-
-            <script src="/app/js-lib/jquery.js"></script>
-            <script src="/app/js-lib/jquery-ui.js"></script>
-
-            <!--<script src="/app/js-lib/jquery.ui.touch-punch.js"></script>-->
-            <script src="/app/js-lib/jquery.mousewheel.js"></script>
-            <script src="/app/js-lib/hammer.js"></script>
-            <script src="/app/js-lib/jquery-fullscreen.js"></script>
+    <link rel="stylesheet" type="text/css"
+          href="/app/css-lib/roboto-fontface.css?family=Roboto:400,700&subset=latin,latin-ext">
+    <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="/app/css-lib/font-awesome-animation.css">
 
 
-            <script src="/app/js/func.lib.js"></script>
-            <script src="/app/js/main.js"></script>
-            <script src="/app/js/lang.lib.js"></script>
-            <script src="/app/locale/cs.js"></script>
+        <script src="/app/js-lib/jquery.js"></script>
+        <script src="/app/js-lib/jquery-ui.js"></script>
+
+        <!--<script src="/app/js-lib/jquery.ui.touch-punch.js"></script>-->
+        <script src="/app/js-lib/jquery.mousewheel.js"></script>
+        <script src="/app/js-lib/hammer.js"></script>
+        <script src="/app/js-lib/jquery.fullscreen.js"></script>
 
 
-            <script src="/app/js/townsapi.lib.js"></script>
-            <script src="/app/js/townsapi.fake.lib.js"></script>
+        <script src="/app/js/func.lib.js"></script>
+        <script src="/app/js/main.js"></script>
+        <script src="/app/js/lang.lib.js"></script>
+        <script src="/app/locale/cs.js"></script>
 
 
-            <script src="/app/js/functions/create.js"></script>
-            <script src="/app/js/functions/terrain.js"></script>
+        <script src="/app/js/townsapi.lib.js"></script>
+        <script src="/app/js/townsapi.fake.lib.js"></script>
 
 
-            <script src="/app/js/map.lib.js"></script>
-            <script src="/app/js/model.lib.js"></script>
-            <script src="/app/js/graphic.js"></script>
-            <script src="/app/js/events.js"></script>
-            <script src="/app/js/objectmenu.js"></script><!--todo soubory podle funkci-->
-            <script src="/app/js/terrainmenu.js"></script>
-            <script src="/app/js/uniquemenu.js"></script>
-            <script src="/app/js/ui.js"></script>
+        <script src="/app/js/functions/create.js"></script>
+        <script src="/app/js/functions/terrain.js"></script>
 
 
-            <script src="app/js/localstorage.fake.js.php"></script>
+        <script src="/app/js/map.lib.js"></script>
+        <script src="/app/js/model.lib.js"></script>
+        <script src="/app/js/graphic.js"></script>
+        <script src="/app/js/events.js"></script>
+        <script src="/app/js/objectmenu.js"></script><!--todo soubory podle funkci-->
+        <script src="/app/js/terrainmenu.js"></script>
+        <script src="/app/js/uniquemenu.js"></script>
+        <script src="/app/js/ui.js"></script>
+
+
+        <script src="app/js/localstorage.fake.js.php"></script>
 
 
 
-            <script src="/app/coffeehtml/debug.js"></script>
-            <!--<script src="app/coffeehtml/loginform.js"></script>-->
-            <script src="/app/coffeehtml/projects.js"></script>
+        <script src="/app/coffeehtml/debug.js"></script>
+        <!--<script src="app/coffeehtml/loginform.js"></script>-->
+        <script src="/app/coffeehtml/projects.js"></script>
 
 
-            <script src="/app/js/functions/login.js"></script>
+        <script src="/app/js/functions/login.js"></script>
 
 
-            <!--<script async src='/browser-sync/browser-sync-client.2.9.8.js'></script>-->
+        <!--<script async src='/browser-sync/browser-sync-client.2.9.8.js'></script>-->
 
 
-        <?php else : ?>
-            <link rel="stylesheet" type="text/css" href="/app-dist/css/towns.css"/>
-            <script src="/app-dist/js/towns.js"></script>
-        <?php endif; ?>
+    <?php else : ?>
+    <link rel="stylesheet" type="text/css" href="/app-dist/css/towns.css"/>
+        <script src="/app-dist/js/towns.js"></script>
+    <?php endif; ?>
 
-    </head>
-    <body>
+</head>
+<body>
 
-        <?php if (isset($config['google']['tracking_id'])) : ?>
-        <script>
+<?php if (isset($config['google']['tracking_id'])) : ?>
+    <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-            ga('create', '<?= $config['google']['tracking_id'] ?>', 'auto');
-            ga('send', 'pageview');
-        </script>
-        <?php endif; ?>
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', '<?= $config['google']['tracking_id'] ?>', 'auto');
+        ga('send', 'pageview');
+    </script>
+<?php endif; ?>
 
 
 
-        <div id="loading">
-            <i class="fa fa-spinner faa-spin animated"></i>
+<div id="loading">
+    <i class="fa fa-spinner faa-spin animated"></i>
+</div>
+
+
+
+<!--start of map-->
+
+<div id="map_drag"></div>
+
+
+<div id="loadbar_outer">
+    <div id="loadbar"></div>
+</div>
+
+<canvas id="map_buffer" width="100" height="100"></canvas>
+<canvas id="map_bg" width="100" height="100"></canvas>
+
+
+<!--end of mapy-->
+
+
+<canvas id="selecting-distance" width="100" height="50"></canvas>
+
+
+<div id="selecting-distance-ctl">
+    <div id="selecting-distance-plus" class="mini-button"><i class="fa fa-plus"></i></div>
+    <div id="selecting-distance-minus" class="mini-button"><i class="fa fa-minus"></i></div>
+    <div id="selecting-distance-left" class="mini-button"><i class="fa fa-angle-double-left"></i></i></div>
+    <div id="selecting-distance-right" class="mini-button"><i class="fa fa-angle-double-right"></i></i></div>
+    <div id="selecting-distance-close" class="mini-button"><i class="fa fa-times"></i></div>
+
+</div>
+
+
+
+
+<nav class="menu">
+
+    <!--todo [PH] vyřešit nějak lépe lokacizaci v aplikaci-->
+    <div class="menu-logo">
+        <img src="media/image/icon/logo1.png" alt="Towns.cz logo"/>
+
+    </div>
+
+
+
+    <ul class="menu-list menu-list-left">
+
+        <li class="menu-list-item">
+            <a href="#">Příroda</a>
+
+            <ul class="menu-dlist">
+                <li class="menu-dlist-item"><a href="#" onclick="objectMenuTerrainChange();">Typy</a></li>
+                <li class="menu-dlist-item"><a href="#" onclick="objectMenuLevelChange();">Výška</a></li>
+            </ul>
+        </li>
+
+        <li class="menu-list-item">
+            <a href="#">Budovy</a>
+
+            <ul class="menu-dlist">
+                <li class="menu-dlist-item"><a href="#" onclick="objectMenuUnique('main');">Zakladni</a></li>
+                <li class="menu-dlist-item"><a href="#" onclick="objectMenuUnique('wall');">Hradby</a></li>
+                <li class="menu-dlist-item"><a href="#" onclick="dismantlingStart();">Zbourat</a></li>
+            </ul>
+        </li>
+
+        <li class="menu-list-item">
+            <a href="#">Příběhy</a>
+
+            <ul class="menu-dlist">
+            </ul>
+        </li>
+
+        <li class="menu-list-item">
+            <a href="#">Zprávy</a>
+
+            <ul class="menu-dlist">
+            </ul>
+        </li>
+
+
+        <li class="menu-list-item">
+            <a href="#">Mapa</a>
+
+            <ul class="menu-dlist">
+            </ul>
+        </li>
+
+
+
+    </ul>
+
+    <ul class="menu-list menu-list-right">
+
+
+        <li class="menu-list-item menu-list-item-registration">
+            <a class="js-popup-window-open" header="Towns 5" content="projects_html" href="#">O hře</a>
+        </li>
+
+
+        <li class="menu-list-item menu-list-item-icon js-popup-notification-open faa-parent animated-hover"><i class="fa fa-flag fa-lg faa-shake"></i></li>
+        <li class="menu-list-item menu-list-item-icon faa-parent animated-hover"><i class="fa fa-wrench fa-lg faa-wrench"></i></li>
+        <li class="menu-list-item menu-list-item-icon faa-parent animated-hover" onclick="$(document).toggleFullScreen();"><i class="fa fa-arrows-alt fa-lg faa-pulse"></i></li><!--todo tohle neni ciste reseni js by mel byt mimo html dokumentu v tomhle pripade v ui.js-->
+
+
+
+    </ul>
+
+    <div class="cleaner"></div>
+</nav>
+
+
+
+
+
+
+
+<aside id="objectmenu">
+    <div id="objectmenu-inner">
+        <div class="action-wrapper">
+
+            <div class="action js-popup-action-open" style="background: url('%icon');background-size: cover;" title="%title" content="%content" onclick="%action"></div>
+            <!--todo [PH] Nevím jak jinak donutit css aby brala v úvahu background-size: cover;-->
+            <!--todo background: url('%icon') nakonec funguje, ale je v logicky spatne, sice to prebere JS ale prohlezec se jednou pokusi nacist nesmyslny obrazek-->
+
         </div>
+        <div>
 
+</aside>
 
 
-        <!--start of map-->
 
-            <div id="map_drag"></div>
+<div id="popup-action">
+    <div class="arrow"></div>
+    <div class="content"></div>
+    <div class="close js-popup-action-close"><i class="fa fa-times"></i></div>
+</div>
 
 
-            <div id="loadbar_outer">
-                <div id="loadbar"></div>
-            </div>
 
-            <canvas id="map_buffer" width="100" height="100"></canvas>
-            <canvas id="map_bg" width="100" height="100"></canvas>
+<div class="overlay" style="display: <?=addslashes($window_display)?>;"></div>
 
+<div class="popup-window" style="display: <?=addslashes($window_display)?>;">
+    <div class="header"><?=addslashes($window_header)?></div>
+    <div class="content"><?=addslashes($window_content)?></div>
 
-        <!--end of mapy-->
 
+    <div class="close js-popup-window-close"><i class="fa fa-times"></i></div>
+</div>
 
-        <canvas id="selecting-distance" width="100" height="50"></canvas>
 
 
-        <div id="selecting-distance-ctl">
-            <div id="selecting-distance-plus" class="mini-button"><i class="fa fa-plus"></i></div>
-            <div id="selecting-distance-minus" class="mini-button"><i class="fa fa-minus"></i></div>
-            <div id="selecting-distance-left" class="mini-button"><i class="fa fa-angle-double-left"></i></i></div>
-            <div id="selecting-distance-right" class="mini-button"><i class="fa fa-angle-double-right"></i></i></div>
-            <div id="selecting-distance-close" class="mini-button"><i class="fa fa-times"></i></div>
 
-        </div>
+<div class="popup-notification">
+    <div class="arrow"></div>
+    <div class="header"></div>
+    <div class="content">
 
+        <?=htmlspecialchars($notifications_content)?>
 
+    </div>
+    <div class="footer">
+        <a href="#">Všetky notifikácie</a>
+    </div>
+</div>
 
 
-        <nav class="menu">
 
-                <!--todo [PH] vyřešit nějak lépe lokacizaci v aplikaci-->
-                <div class="menu-logo">
-                    <img src="media/image/icon/logo1.png" alt="Towns.cz logo"/>
-
-                </div>
-
-
-
-                <ul class="menu-list menu-list-left">
-
-                    <li class="menu-list-item">
-                        <a href="#">Příroda</a>
-
-                        <ul class="menu-dlist">
-                            <li class="menu-dlist-item"><a href="#" onclick="objectMenuTerrainChange();">Typy</a></li>
-                            <li class="menu-dlist-item"><a href="#" onclick="objectMenuLevelChange();">Výška</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="menu-list-item">
-                        <a href="#">Budovy</a>
-
-                        <ul class="menu-dlist">
-                            <li class="menu-dlist-item"><a href="#" onclick="objectMenuUnique('main');">Zakladni</a></li>
-                            <li class="menu-dlist-item"><a href="#" onclick="objectMenuUnique('wall');">Hradby</a></li>
-                            <li class="menu-dlist-item"><a href="#" onclick="dismantlingStart();">Zbourat</a></li>
-                        </ul>
-                    </li>
-
-                    <li class="menu-list-item">
-                        <a href="#">Příběhy</a>
-
-                        <ul class="menu-dlist">
-                        </ul>
-                    </li>
-
-                    <li class="menu-list-item">
-                        <a href="#">Zprávy</a>
-
-                        <ul class="menu-dlist">
-                        </ul>
-                    </li>
-
-
-                    <li class="menu-list-item">
-                        <a href="#">Mapa</a>
-
-                        <ul class="menu-dlist">
-                        </ul>
-                    </li>
-
-
-
-                </ul>
-
-                <ul class="menu-list menu-list-right">
-
-
-                    <li class="menu-list-item menu-list-item-registration">
-                        <a class="js-popup-window-open" header="Towns 5" content="projects_html" href="#">O hře</a>
-                    </li>
-
-
-                    <li class="menu-list-item menu-list-item-icon js-popup-notification-open faa-parent animated-hover"><i class="fa fa-flag fa-lg faa-shake"></i></li>
-                    <li class="menu-list-item menu-list-item-icon faa-parent animated-hover"><i class="fa fa-wrench fa-lg faa-wrench"></i></li>
-                    <li class="menu-list-item menu-list-item-icon faa-parent animated-hover" onclick="$(document).fullscreen().toggle();"><i class="fa fa-arrows-alt fa-lg faa-pulse"></i></li><!--todo tohle neni ciste reseni js by mel byt mimo html dokumentu v tomhle pripade v ui.js-->
-
-
-
-                </ul>
-
-                <div class="cleaner"></div>
-            </nav>
-
-
-
-
-
-
-
-            <aside id="objectmenu">
-                <div id="objectmenu-inner">
-                    <div class="action-wrapper">
-
-                        <div class="action js-popup-action-open" style="background: url('%icon');background-size: cover;" title="%title" content="%content" onclick="%action"></div>
-                        <!--todo [PH] Nevím jak jinak donutit css aby brala v úvahu background-size: cover;-->
-                        <!--todo background: url('%icon') nakonec funguje, ale je v logicky spatne, sice to prebere JS ale prohlezec se jednou pokusi nacist nesmyslny obrazek-->
-
-                    </div>
-                <div>
-
-            </aside>
-
-
-
-            <div id="popup-action">
-                <div class="arrow"></div>
-                <div class="content"></div>
-                <div class="close js-popup-action-close"><i class="fa fa-times"></i></div>
-            </div>
-
-
-
-            <div class="overlay" style="display: <?=addslashes($window_display)?>;"></div>
-
-            <div class="popup-window" style="display: <?=addslashes($window_display)?>;">
-                <div class="header"><?=addslashes($window_header)?></div>
-                <div class="content"><?=addslashes($window_content)?></div>
-
-
-                <div class="close js-popup-window-close"><i class="fa fa-times"></i></div>
-            </div>
-
-
-
-
-            <div class="popup-notification">
-                <div class="arrow"></div>
-                <div class="header"></div>
-                <div class="content">
-
-                    <?=htmlspecialchars($notifications_content)?>
-
-                </div>
-                <div class="footer">
-                    <a href="#">Všetky notifikácie</a>
-                </div>
-            </div>
-
-
-
-    </body>
+</body>
 </html>
--- gulpfile.js
+++ gulpfile.js
@@ -5,50 +5,10 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     imagemin = require('gulp-imagemin'),
     cache = require('gulp-cache'),
-    jsdoc = require("gulp-jsdoc"),
+//jsdoc = require("gulp-jsdoc"),
     rename = require('gulp-rename'),
     del = require('del'),
     config = require('./config/app.json');
-
-// Index.php pre produkcny build
-gulp.task('production-index', function () {
-    gulp.src('app/*.php')
-        .pipe(gulp.dest('app-dist/'));
-});
-
-// Scripts
-gulp.task('production-scripts', function() {
-    gulp.src('app/js/*.js')
-        .pipe(concat('towns.js'))
-        .pipe(gulp.dest('app-dist/js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest('app-dist/js'));
-});
-
-// Styly
-gulp.task('production-styles', function () {
-    gulp.src('app/**/*.css')
-        .pipe(concat('towns.css'))
-        .pipe(gulp.dest('app-dist/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('app-dist/css'));
-});
-
-// Obrazky - nateraz neaktivne
-gulp.task('production-images', function () {
-    gulp.src('media/image/**/*')
-        .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true, multipass: true })))
-        //.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true, multipass: true }))
-        .pipe(gulp.dest('app-dist/media/image'));
-});
-
-// Zvuky - nateraz neaktivne
-gulp.task('production-sound', function () {
-    gulp.src('media/sound/*')
-        .pipe(gulp.dest('app-dist/media/sound'));
-});
 
 // Lint - testovanie
 gulp.task("test", function() {
@@ -105,7 +65,8 @@ gulp.task('develop-scripts', function() {
         'node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch.js',
         'node_modules/jquery-mousewheel/jquery.mousewheel.js',
         'node_modules/hammerjs/hammer.js',
-        'node_modules/jquery-fullscreen/jquery-fullscreen.js'])
+        'node_modules/jquery-fullscreen-plugin/jquery.fullscreen.js'
+    ])
         .pipe(gulp.dest('app/js-lib/'));
 });
 
@@ -168,11 +129,11 @@ gulp.task('production', ['production-clean'], function() {
 gulp.task('production-clean', function() {
     del([
         'app-dist/index.php',
-        'app-dist/css',
+        'app-dist/css/*',
         //'app-dist/media/image',
         //'app-dist/media/sound',
-        'app-dist/js'
-
+        'app-dist/js/*',
+        'app-dist/fonts/*'
     ])
 });
 
@@ -182,7 +143,97 @@ gulp.task('production-build', [
     'production-scripts',
     //'production-images',
     //'production-sound',
-    'production-styles'
+    'production-styles',
+    'production-fonts',
+    'production-graphic'
 ], function () {
     console.log(' ¯\\_(ツ)_/¯ Produkčný build je hotový ');
+});
+
+// Index.php pre produkcny build
+gulp.task('production-index', function () {
+    gulp.src('app/*.php')
+        .pipe(gulp.dest('app-dist/'));
+});
+
+// Scripts - musia byt vylistovane radsej ako nacitanim s wildcard pretoze poradie nacitania zalezi
+gulp.task('production-scripts', function() {
+    gulp.src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/jquery-ui-bundle/jquery-ui.js',
+        //'node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch.js',
+        'node_modules/jquery-mousewheel/jquery.mousewheel.js',
+        'node_modules/hammerjs/hammer.js',
+        'node_modules/jquery-fullscreen-plugin/jquery.fullscreen.js',
+        'app/js/func.lib.js',
+        'app/js/main.js',
+        'app/js/lang.lib.js',
+        'app/locale/cs.js',
+        'app/js/townsapi.lib.js',
+        'app/js/townsapi.fake.lib.js',
+        'app/js/functions/create.js',
+        'app/js/functions/terrain.js',
+        'app/js/map.lib.js',
+        'app/js/model.lib.js',
+        'app/js/graphic.js',
+        'app/js/events.js',
+        'app/js/objectmenu.js',
+        'app/js/terrainmenu.js',
+        'app/js/uniquemenu.js',
+        'app/js/ui.js',
+        //'app/js/localstorage.fake.js.php',
+        'app/coffeehtml/debug.js',
+        //'app/coffeehtml/loginform.js',
+        'app/coffeehtml/projects.js',
+        'app/js/functions/login.js'
+        //'browser-sync/browser-sync-client.2.9.8.js'
+    ])
+        .pipe(concat('towns.js'))
+        .pipe(gulp.dest('app-dist/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('app-dist/js'));
+});
+
+// Styly - musia byt vylistovane radsej ako nacitanim s wildcard pretoze poradie nacitania zalezi
+gulp.task('production-styles', function () {
+    gulp.src([
+        'app/css/*.css',
+        'node_modules/roboto-fontface/css/roboto-fontface.css',
+        'node_modules/font-awesome/css/font-awesome.css',
+        'node_modules/font-awesome-animation/src/font-awesome-animation.css'])
+        .pipe(concat('towns.css'))
+        .pipe(gulp.dest('app-dist/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('app-dist/css'));
+});
+
+// Obrazky - nateraz neaktivne
+gulp.task('production-images', function () {
+    gulp.src('media/image/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true, multipass: true })))
+        //.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true, multipass: true }))
+        .pipe(gulp.dest('app-dist/media/image'));
+});
+
+// Zvuky - nateraz neaktivne
+gulp.task('production-sound', function () {
+    gulp.src('media/sound/*')
+        .pipe(gulp.dest('app-dist/media/sound'));
+});
+
+// Priprav fonty pre produkčný build
+gulp.task('production-fonts', function () {
+    gulp.src([
+        'node_modules/roboto-fontface/fonts/*',
+        'node_modules/font-awesome/fonts/*'])
+        .pipe(gulp.dest('app-dist/fonts/'));
+});
+
+// Priprav graphic pre produkčný build
+gulp.task('production-graphic', function () {
+    gulp.src([
+        'app/graphic/*'])
+        .pipe(gulp.dest('app-dist/graphic/'));
 });
