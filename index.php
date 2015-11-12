d3b5a8b5802a4fb499e4663c74e9cfaee8c5cd93
Stefan Kecskes <mr.kecskes@gmail.com>
2015-11-12 14:15:22 +0000

    [FRONTEND][SK] added configurations autoloader for gulp and php. All .json configuration files in ./config folder are now automagically loaded into config variable

--- index.php
+++ index.php
@@ -16,11 +16,20 @@
 //======================================================================================================================
 
 
+// Config autoloader - get all files from config folder
+foreach(scandir(__DIR__."/config") as $config_file)
+{
+    // and if the file is .json, then
+    if (($offset = strlen($config_file) - strlen(".json")) >= 0 && strpos($config_file, ".json", $offset) !== FALSE)
+    {
+        // load it to config array
+        $name = substr($config_file, 0, $offset);
+        $config[$name] = json_decode(file_get_contents(__DIR__."/config/".$config_file), true);
+    }
+}
 
-$config = json_decode(file_get_contents(__DIR__."/config/app.json"), true);
-
-// Na zaklade konfiguracie rozhodni ktoru verziu budes spustat
-if(isset($config['environment']) && in_array($config['environment'], ["develop", "test"])){
+// load the app based on configuration environment
+if(isset($config['app']['environment']) && in_array($config['app']['environment'], ["develop", "test"])){
     require('app/index.php');
 } else {
     require('app-dist/index.php');
