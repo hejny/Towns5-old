75f95eb73daf20d23087da682b657943961636b0
Pavol Hejný <ph@towns.cz>
2015-11-18 21:43:24 +0100

    [App][PH] Story editor - simple

--- app/pages/storyeditor.js
+++ app/pages/storyeditor.js
@@ -7,59 +7,28 @@ pages.storyeditor={};
 pages.storyeditor.html=[
     '<input type="text" id="story-name" value="" placeholder="Název příběhu">',
     '<button id="story-save" onclick="storySave();">Uložit</button>',
-    '<div id="story-content">cccc</div>'
-].join('');
+    '<br>',
+    '<textarea id="story-content"></textarea>',
+''].join('');
 
 
-var story_editor;
+
+
+
 
 pages.storyeditor.js = function(){
 
 
+    var story_name='Nazev';
+    var story_content=[
+            'Nadpis',
+            '=========',
+            '',
+            'text text text',
+        ''].join('\n');
 
-    var opts = {
-        container: 'story-content',
-        textarea: null,
-        basePath: 'node_modules/epiceditor/epiceditor/',
-        clientSideStorage: true,
-        localStorageName: 'epiceditor',
-        useNativeFullsreen: true,
-        parser: marked,
-        file: {
-            name: 'epiceditor',
-            defaultContent: 'ahoj',
-            autoSave: 100
-        },
-        theme: {
-            base: 'themes/base/epiceditor.css',
-            preview: '../../../app/other/preview-light.css',
-            editor: 'themes/editor/epic-light.css'
-            /*preview: '/themes/preview/preview-dark.css',
-            editor: '/themes/editor/epic-dark.css'*/
-        },
-        button: {
-            preview: true,
-            fullscreen: false
-        },
-        focusOnLoad: false,
-        shortcut: {
-            modifier: 18,
-            fullscreen: 70,
-            preview: 80
-        },
-        string: {
-            togglePreview: 'Toggle Preview Mode',
-            toggleEdit: 'Toggle Edit Mode',
-            toggleFullscreen: 'Enter Fullscreen'
-        }
-    };
-
-
-    story_editor = new EpicEditor(opts).load();
-
-    var elements=['container','wrapper','wrapperIframe','editor','editorIframe','previewer','previewerIframe'];
-    for(var key in elements)
-        $(story_editor.getElement(elements[key])).css('height',window.innerHeight-300);
+    $('#story-content').val(story_name);
+    $('#story-content').val(story_content);
 
 
 };
@@ -69,8 +38,9 @@ pages.storyeditor.js = function(){
 function storySave(){
 
     var story_name = $('#story-name').val();
-    var story_content = story_editor.getElement('editor').body.innerHTML;
+    var story_content = $('#story-content').val();
+    var story_content_html = markdown.toHTML(story_content);
 
-    r(story_name,story_content);
+    r(story_name,story_content,story_content_html);
 
 }
