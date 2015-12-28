0f5a3636a3de3d4b3ebb3f29cd8a3d8b8d235077
phejny <ph@towns,cz>
2015-12-28 22:40:26 +0100

    [app][PH] Help popup

--- app/js/classes-static/templates.class.js
+++ app/js/classes-static/templates.class.js
@@ -25,7 +25,7 @@ Templates.objectMenu = function(params){
         <div class="action js-popup-action-open"`+
 
             (defined(params.icon)?` style="background: url('`+params.icon+`');background-size: cover;"`:``)+
-            (defined(params.title)?` title="`+params.title+`"`:``)+
+            (defined(params.title)?` popup_title="`+params.title+`"`:``)+//todo towns:title as xml namespace
             (defined(params.content)?` content="`+params.content+`"`:``)+
             (defined(params.action)?` action="`+params.action+`"`:``)+`>`+
             (defined(params.inner)?params.inner:``)+
