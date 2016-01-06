17a1e2d6ac40f3ecbb039cb8da162944a42fe5a0
Pavol Hejný <ph@towns.cz>
2016-01-06 15:31:26 +0100

    [App][PH]  Map / better map generator and TDD

--- app/tdd/model.php
+++ app/tdd/model.php
@@ -1,42 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>TDD Model</title>
 
 
-    <?php
-
+<?php
+    $title='model';
     $inits=array('objects-prototypes.init.js');
+    require('init.php');
+?>
 
-    //--------------------------------Includes
-
-    $includes = json_decode(file_get_contents(__DIR__."/../../config/includes.json"), true);
-    foreach ($includes['js'] as $include) {
-
-
-
-        if(is_array($include)){
-            foreach($include as $environment=>$file){
-                if('develop'==$environment){
-                    echo '<script src="/' . addslashes($file) . '"></script>'."\n";
-                }
-            }
-        }elseif(is_string($include)){
-
-            if((strpos($include,'inits/')===false and strpos($include,'ui/')===false)/**/ or in_array(basename($include),$inits)/**/)
-                echo '<script src="/' . addslashes($include) . '"></script>'."\n";
-
-        }
-
-    }
-    //--------------------------------
-
-    ?>
-
-
-</head>
-<body>
 
 
 <canvas id="canvas1" width="300" height="300"></canvas>
