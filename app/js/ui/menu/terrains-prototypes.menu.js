c9d659f368e8c4cf54294b6aace6185a74bf3452
Pavol Hejný <ph@towns.cz>
2016-01-16 03:21:55 +0100

    [APP][PH]  API / terrains as prototypes

--- app/js/ui/menu/terrains-prototypes.menu.js
+++ app/js/ui/menu/terrains-prototypes.menu.js
@@ -42,13 +42,15 @@ function terrainNeutralizeStop(){
 //======================================================================================================================objectMenuTerrainChange
 //CHANGE
 
-function terrainChangeStart(terrain){//todo here should be prototype id
+function terrainChangeStart(prototypeId){
 
     mapSpecialCursorStart();
 
     updateSelectingDistance();
 
-    terrainChanging={
+    terrainChanging=deepCopyObject(ArrayFunctions.id2item(object_prototypes,prototypeId));
+
+    /*terrainChanging={
         "type": "terrain",
         "design": {
             "type": "terrain",
@@ -57,7 +59,7 @@ function terrainChangeStart(terrain){//todo here should be prototype id
                 "size": 1//todo maybe this (terrains and buildings) should be radius
             }
         }
-    };
+    };*/
 
     //----------------------------Dismantling by terrain changing eg. when changing to water, all building are dismantled
     if(blockedTerrains.indexOf(terrainChanging)!=-1){
@@ -66,7 +68,7 @@ function terrainChangeStart(terrain){//todo here should be prototype id
     //----------------------------
 
     //if(terrain_change){
-    $('#selecting-distance-ctl').css('background','url(\''+appDir+'/php/terrain.php?raw&size=200&terrain=t'+(terrain)+'\')');
+    $('#selecting-distance-ctl').css('background','url(\''+appDir+'/php/terrain.php?raw&size=200&terrain=t'+(terrainChanging.design.data.image)+'\')');
     $('#selecting-distance-ctl').css('background-size','cover');
     //}
 
@@ -87,54 +89,6 @@ function terrainChangeStart(terrain){//todo here should be prototype id
 function terrainChangeStop(){
 
     terrainChanging=false;
-
-}
-
-
-
-
-//======================================================================================================================
-//MENU
-
-
-
-function objectMenuTerrainChange(){//todo refactor this should be under objectMenu
-
-    var objectmenu='';
-
-        for(var terrain=1;terrain<14;terrain++){
-
-
-            var icon=appDir+'/php/terrain.php?raw&size=60&terrain=t'+(terrain);
-
-            /*content='<h2>'+l('terrain','t'+terrain)+'</h2>' +
-                '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio eligendi et ex fuga mollitia nisi obcaecati possimus sint, tenetur vitae? A aspernatur officiis quas quis ratione. Atque fugit optio suscipit?</p> ' +
-                '<button>Postaviť drist!</button>';*/
-            var content='';
-            var action='terrainChangeStart('+(terrain)+',false);';
-
-
-            objectmenu+=Templates.objectMenu({
-                icon: icon,
-                icon_size: 1.2,
-                selectable: true,
-                title: Locale.get('terrain '+terrain),
-                content: Locale.get('terrain '+terrain+' description'),
-                action: action
-            });
-
-
-
-            //$(objectmenu[i]).children('div').attr('content',content);
-            //$(objectmenu[i]).children('.js-popup-action-open').scss('background','url(\''+icon+'\')');
-
-
-
-        }
-
-
-    showLeftMenu(objectmenu);
-
 
 }
 
